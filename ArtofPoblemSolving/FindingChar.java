7d3698d41e6d2a5b16c458f9b19ee0ab518c883b
bala472 <160371380+bala472@users.noreply.github.com>
2024-03-16 15:35:15 +0530

    Update FindingChar.java

--- ArtofPoblemSolving/FindingChar.java
+++ ArtofPoblemSolving/FindingChar.java
@@ -1,7 +1,14 @@
-public class FindingChar {
+import java.util.Scanner;
+
+class FindingChar {
     public static void main(String[] args) {
+        System.out.println("Enter a word (eg. Hello) : ");
+        Scanner in = new Scanner(System.in);
+        String word = in.nextLine();
+        System.out.println("Enter a single character to search in the word (eg. l): ");
+        String search = in.nextLine();
         FindingChar obj = new FindingChar();
-        System.out.println(obj.isContainsChar("Yellow",'u'));
+        System.out.println("Result : "+obj.isContainsChar(word,search.charAt(0)));
     }
     public boolean isContainsChar(String str,char i){
         if(str.charAt(0)==i){
